51a863f51f6e5586fef5a56e021fff0781e5ddfd
Peashooter101 <Peashooter101@yahoo.com>
2024-01-02 20:33:36 -0800

    Commit Regularly and Often
    Initial Commit, Empty Mod
    
    Praying for mercy from csproj.

---
+++ README.md
@@ -0,0 +1,2 @@
+# SimplyLethalCustomization
+ A Lethal Company Mod focusing on fully configuring vanilla features and scaling.
---
+++ SimplyLethalCustomization/SimplyLethalCustomization/Main.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyLethalCustomization
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SimplyLethalCustomizationBase : BaseUnityPlugin
+    {
+        private const string modGUID = "SimplexityDev.SimplyLethalCustomization";
+        private const string modName = "Simply Lethal Customization";
+        private const string modVersion = "0.0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static SimplyLethalCustomizationBase instance;
+
+        internal ManualLogSource logger;
+
+        void Awake()
+        {
+            if (instance == null) instance = this;
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            harmony.PatchAll();
+        }
+    }
+}
